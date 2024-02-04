d9bc5e4ac12cd32491c72742545b9c94a37c7312
neon-leitz <neon.leitz99@gmail.com>
2024-02-04 10:40:04 -0500

    Manually allocate upload counts from 'Ayutac' to 'lightningowl'

--- stats/static/js/charts/uploaders.js
+++ stats/static/js/charts/uploaders.js
@@ -1,9 +1,22 @@
 import * as Config from "../default_config.js"
 import uploader_counts from "../data/post_uploader_counts.json" assert {type: "json"};
 
+// Manually transfer upload counts from ayutac to lightningowl
+const TRANSFER_COUNT = 336
+if (uploader_counts["lightningowl"] === undefined) {
+  uploader_counts["lightningowl"] = 0;
+}
+
+if (uploader_counts["Ayutac"]) {
+  uploader_counts["Ayutac"] -= TRANSFER_COUNT
+  uploader_counts["lightningowl"] += TRANSFER_COUNT
+}
+
+let counts = Object.entries(uploader_counts).sort((a, b) => b[1] - a[1])
+
 let data = {
-  y: Object.keys(uploader_counts).slice(0, 20).reverse(),
-  x: Object.values(uploader_counts).slice(0, 20).reverse(),
+  y: counts.map(x => x[0]).slice(0, 15).reverse(),
+  x: counts.map(x => x[1]).slice(0, 15).reverse(),
   marker: {
     color: Config.default_bar_colors_medium,
   },
